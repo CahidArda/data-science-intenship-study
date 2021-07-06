d719d6dc3952749cdaf61f5fd82901c2a6ddd883
CahidArda <cahidardaooz@hotmail.com>
2021-07-06 11:50:24 +0300

    Add compare_model_parameter method
    Also change get_weekly_error to get_error_with_freq

--- notebooks/atm_eda/forecasting.py
+++ notebooks/atm_eda/forecasting.py
@@ -12,6 +12,7 @@ def mape_error(y_actual, y_pred, mean=True):
 
 # input:            pandas dataframe representing the target variables, size of the window (must be higher than the highest value in averages parameter)
 # input example:    average=[[7,14], [7,14,21]] calculate the averages of t-7 and t-14, then find MAPE with this average. Same for t-7, t-14 and t-21
+# do:               Calculate errors by shifting and averaging. These errors are then used as base errors
 def get_shifted_errors(y, size, average=[]):
     shifted_errors = pd.DataFrame(dtype='float64')
     for target in y.columns:
@@ -27,18 +28,64 @@ def get_shifted_errors(y, size, average=[]):
     return shifted_errors 
 
 # input: two pandas series representing the predictions and actual values
-def get_weekly_error(y_actual, y_pred, error_freq):
-    weekly_errors = mape_error(y_actual, y_pred, mean=False)
-    weekly_errors.dropna(inplace=True)
-    weekly_errors = weekly_errors.resample(error_freq).mean()
-    return weekly_errors
+# do:    calculate the error and average it with the given frequency
+def get_error_with_freq(y_actual, y_pred, error_freq='w'):
+    errors = mape_error(y_actual, y_pred, mean=False)
+    errors.dropna(inplace=True)
+    errors = errors.resample(error_freq).mean()
+    return errors
+
+# ------------------------------------------
+# 
+# ------------------------------------------
+
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+# input:
+#   - algorithm: ML algortihm to train and test
+#   - X: feature dataframe
+#   - y: target series
+#   - parameter: parameter to test and draw on 2D plot
+#   - values: range of values in a list
+# do:   iterate over the values list and train the model with each parameter instance.
+#       Test these models and plot the error
+def compare_model_parameter(algorithm, X, y, parameter, values, shuffle=True):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=shuffle)
+
+    train_error = []
+    test_error = []
+    best = [0, 100]
+    values.sort()
+    for value in values:
+        d = {parameter: value}
+        model = algorithm(**d, random_state=5)
+        model.fit(X_train, y_train)
+
+        train_error.append(mape_error(model.predict(X_train), y_train))
+        test_error.append(mape_error(model.predict(X_test), y_test))
+
+        if test_error[-1] < best[1]:
+            best = [value, test_error[-1]]
+
+
+    plt.plot(values, train_error, label='Training error')
+    plt.plot(values, test_error, label='Testing error')
+    plt.legend()
+    plt.show()
+
+    print("Best test error: %.3f with %d trees."%(best[1], best[0]))
+
+# ------------------------------------------
+# Plotting error, predictions and actuals
+# ------------------------------------------
 
 # input:    Trained model, data used to train the model, actual values
 # do:       Using the model and data, draw actual/predicted and the error over time
 def draw_model_error(model, X, y_actual, error_freq='w', split_from=None):
 
     y_pred = pd.Series(model.predict(X), index=X.index)
-    weekly_errors = get_weekly_error(y_actual, y_pred, error_freq)
+    weekly_errors = get_error_with_freq(y_actual, y_pred, error_freq)
     draw_error_over_time(y_actual, y_pred, weekly_errors, split_from)
 
 def draw_error_over_time(y_actual, y_pred, weekly_errors, split_from=None):
